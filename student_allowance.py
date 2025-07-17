b77b5fc3f6fe0ae82289ae3f74aa9653553ad98a
Njien <thomasjessyendi@gmail.com>
2025-07-17 17:18:08 +0100

    second commit

---
+++ student_allowance.py
@@ -0,0 +1,20 @@
+weekly_allowance = 2000
+
+weekly_allowance -= 400
+
+weekly_allowance += 100
+
+weekly_allowance -= 250
+
+percentage = (25 / 100) * weekly_allowance 
+weekly_allowance -= percentage
+onethird = weekly_allowance / 3
+
+weekly_allowance -= onethird
+
+weekly_allowance //= 2
+
+weekly_allowance %= 100
+
+
+print(weekly_allowance)
